3898002a4e3dc912547a1df7592a1d8efc151bac
Toritou <l.toro05@ufromail.cl>
2024-05-08 19:32:10 -0400

    codigos de visual paradigm

---
+++ src/Calculadora.java
@@ -0,0 +1,29 @@
+public class Calculadora {
+    private int n1;
+    private int n2;
+
+    public Calculadora() {
+        // Constructor de la clase
+    }
+
+    public Calculadora(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public int suma() {
+        return n1 + n2;
+    }
+
+    public int multiplicar() {
+        return n1 * n2;
+    }
+
+    public void setN1(int n1) {
+        this.n1 = n1;
+    }
+
+    public void setN2(int n2) {
+        this.n2 = n2;
+    }
+}
---
+++ src/CarroCompra.java
@@ -0,0 +1,19 @@
+public class CarroCompra {
+    private int[][] productos;
+    private Calculadora calculadora;
+
+    public CarroCompra() {
+        calculadora = new Calculadora();
+    }
+
+    public int CalculadoraTotal() {
+        return 0;
+    }
+
+    public int subTotal(int cant, int precio) {
+        return 0;
+    }
+
+    public void mostraTotal() {
+    }
+}
